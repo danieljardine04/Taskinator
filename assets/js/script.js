c98801029b668d4a57446e7b00e0df96f2faac62
danieljardine04 <danogaryjardine@gmail.com>
2021-11-12 11:08:53 -0700

    fixing the submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 var buttonE1 = document.querySelector("#save-task");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
 var createTaskHandler = function(){
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task";
     tasksToDoE1.appendChild(listItemEl);
+    console.log(event);
     
 }
 
-buttonE1.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
